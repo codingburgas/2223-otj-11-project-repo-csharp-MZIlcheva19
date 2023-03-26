862e31e4058347035d8091e9278121906bde169a
MZIlcheva19 <MZIlcheva19@codingburgas.bg>
2023-03-26 10:39:50 +0300

    Connect to database & fix solution file

---
+++ SalonManagement/SalonManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SalonManagement.Startup))]
+namespace SalonManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
